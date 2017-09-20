a07d783a8c397325089c757dd186147eba9cafb9
WillRichey1234 <gsampson@vms.edu>
2017-09-20 11:45:52 -0600

    Branching Part2

---
+++ BranchingPart2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BranchingPart2
+{
+    class MainClass
+    {
+        //METHODS = FUNCTIONS IN CODE HS
+        public static void Main(string[] args)
+        {
+            //string myValue = superSecretFormula("world");
+            // this private static string allows me to write the word hello in front of other words
+            //so that I do not have to write the same code twice
+            string myValue = superSecretFormula("sunshine");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+                              
+        }
+        // this is like a private function in codehs
+        // i can call it anywhere and run it a bunch so i do not have to write code twice
+        // void means it will not return anything like in public static void
+
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello, {0}!", name);
+        }
+    }
+}
